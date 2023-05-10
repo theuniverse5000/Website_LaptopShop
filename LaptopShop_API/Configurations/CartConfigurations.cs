273a419732917b1a2583776ddc786f77f0daa4be
minh <theuniverse5000@gmail.com>
2023-05-10 10:17:35 +0700

    Configuration for Database

---
+++ LaptopShop_API/Configurations/CartConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using LaptopShop_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LaptopShop_API.Configurations
+{
+    public class CartConfigurations : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("Cart");
+            builder.HasKey(p => p.UserId);
+            builder.Property(p => p.Description).
+                HasColumnType("nvarchar(100)");
+        }
+    }
+}
---
+++ LaptopShop_API/Configurations/SsdConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using LaptopShop_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LaptopShop_API.Configurations
+{
+    public class SsdConfigurations : IEntityTypeConfiguration<Ssd>
+    {
+        public void Configure(EntityTypeBuilder<Ssd> builder)
+        {
+            builder.ToTable("SSD");
+            builder.HasKey(p => p.Id);
+            builder.Property(a => a.Ma).IsRequired();
+            builder.Property(a => a.ThongSo).HasColumnName("ThongSo").HasColumnType("varchar(70)").IsRequired();
+
+        }
+    }
+}
