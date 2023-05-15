217a7e6e8fed54cf62b1bbd8b8166a2e0dae476a
Quyvv26931 <118253878+Quyvv26931@users.noreply.github.com>
2023-05-15 21:11:08 +0700

    Quý
    Services

---
+++ Data/Services/Implements/UserServices.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Services.Interfaces;
+using Data.Models;
+using Data.Models;
+using Data.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Data.Services.Implements
+{
+    public class UserServices : IUserServices
+    {
+        ApplicationDbContext _context;
+        public UserServices()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public async Task<bool> Add(User user)
+        {
+            try
+            {
+                user.Id = Guid.NewGuid();
+                await _context.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            try
+            {
+                var listObj = await _context.Users.ToListAsync();
+                var temp = listObj.FirstOrDefault(v => v.Id == id);
+
+                _context.Remove(temp);
+                await Task.FromResult<User>(_context.Users.Remove(temp).Entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<User>> GetAllUser()
+        {
+            var listObj = await _context.Users.ToListAsync();
+            if (listObj == null)
+            {
+                return new();
+            }
+            return listObj;
+        }
+
+        public async Task<User> GetById(Guid id)
+        {
+            var listObj = await _context.Users.ToListAsync();
+            var temp = listObj.FirstOrDefault(v => v.Id == id);
+
+            if (temp == null)
+            {
+                return new User();
+            }
+            return temp;
+        }
+
+        public async Task<List<string>> GetByName(string name)
+        {
+            var listObj = await _context.Users.ToListAsync();
+            var temp = listObj.FirstOrDefault(v => v.Equals(name));
+
+            if (listObj == null)
+            {
+                return null;
+            }
+            return new List<string>();
+        }
+
+        public async Task<bool> Update(User user, Guid id)
+        {
+            try
+            {
+                var listObj = await _context.Users.ToListAsync();
+                var temp = listObj.FirstOrDefault(v => v.Id == id);
+
+                _context.Update(temp);
+                await Task.FromResult<User>(_context.Users.Update(temp).Entity);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Data/Services/Interfaces/IScreenServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services.Interfaces
+{
+    public interface IScreenServices
+    {
+        Task<List<Screen>> GetAllScreen();
+        Task<bool> Update(Screen screen, Guid id);
+        Task<bool> Delete(Guid id);
+        Task<bool> Add(Screen screen);
+        Task<Screen> GetById(Guid id);
+        Task<List<string>> GetByName(string name);
+    }
+}
