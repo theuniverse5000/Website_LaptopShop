b1362416c4c70faf2b401dde7086178f3c88d008
minh <theuniverse5000@gmail.com>
2023-05-16 15:37:08 +0700

    Add DI

--- LaptopShop_API/Program.cs
+++ LaptopShop_API/Program.cs
@@ -1,10 +1,19 @@
 ﻿
+using Data.Services.Implements;
+using Data.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IBillServices, BillServices>();
+builder.Services.AddTransient<IBillDetailServices, BillDetailServices>();
+builder.Services.AddTransient<ICartServices, CartServices>();
+builder.Services.AddTransient<ICartDetailServices, CartDetailServices>();
+builder.Services.AddTransient<IProductDetailServices, ProductDetailServices>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
